a552a2a7cc1aca1d232353f5e5d9051db5c92f86
anthonyhirusha9 <aghirusha@gmail.com>
2025-06-03 18:53:10 +0200

    Delete nimbus-transfers directory

--- nimbus-transfers/components/ui/button.tsx
+++
@@ -1,4 +0,0 @@
-
-export function Button({ children, className }: any) {
-  return <button className={`rounded-xl px-4 py-2 ${className}`}>{children}</button>;
-}
--- nimbus-transfers/components/ui/card.tsx
+++
@@ -1,7 +0,0 @@
-
-export function Card({ children }: any) {
-  return <div className='rounded-2xl shadow p-4 bg-gray-900'>{children}</div>;
-}
-export function CardContent({ children }: any) {
-  return <div>{children}</div>;
-}
